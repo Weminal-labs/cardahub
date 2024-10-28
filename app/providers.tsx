8ae195e1c852304adf0a60ab78f83c82a3e19043
Menhythien <dinhthienmenh1505@gmail.com>
2024-10-28 23:54:18 +0700

    add unique chains

--- app/providers.tsx
+++ app/providers.tsx
@@ -11,6 +11,8 @@ import {
   ledgerWallet,
 } from '@rainbow-me/rainbowkit/wallets';
 import {
+  klaytn,
+  klaytnBaobab,
   mainnet, // import ethereum mainnet
   sepolia, // import sepolia testnet
 } from 'wagmi/chains';
@@ -20,7 +22,81 @@ import { WagmiProvider, http } from 'wagmi';
 
 const { wallets } = getDefaultWallets();
 // khởi tạo và destructure wallets object
-
+// Define the Unique chain
+const uniqueChain = {
+  id: 8880, // Sửa thành 8880 hoặc "0x22b0"
+  name: "Unique",
+  network: "unique",
+  nativeCurrency: {
+    name: "Unique",
+    symbol: "UNQ",
+    decimals: 18,
+  },
+  rpcUrls: {
+    default: {
+      http: ["https://rpc.unique.network"],
+      websocket: ["wss://ws.unique.network"],
+    },
+    public: {
+      http: ["https://rpc.unique.network"],
+      websocket: ["wss://ws.unique.network"],
+    },
+  },
+  blockExplorers: {
+    default: { name: "UniqueScan", url: "https://unique.subscan.io/" },
+  },
+  testnet: false,
+};
+// Define the Unique chain
+const quartzMainnet = {
+  id: 8881, // Sửa thành 8880 hoặc "0x22b0"
+  name: "Quartz",
+  network: "quartz",
+  nativeCurrency: {
+    name: "Quartz",
+    symbol: "QTZ",
+    decimals: 18,
+  },
+  rpcUrls: {
+    default: {
+      http: ["https://rpc-quartz.unique.network"],
+      websocket: ["wss://ws-quartz.unique.network"],
+    },
+    public: {
+      http: ["https://rpc-quartz.unique.network"],
+      websocket: ["wss://ws-quartz.unique.network"],
+    },
+  },
+  blockExplorers: {
+    default: { name: "Quartz", url: "https://quartz.subscan.io/" },
+  },
+  testnet: false,
+};
+// Define the Unique chain
+const opalTestnet = {
+  id: 8882, // Sửa thành 8880 hoặc "0x22b0"
+  name: "Opal",
+  network: "opal",
+  nativeCurrency: {
+    name: "Opal",
+    symbol: "OPL",
+    decimals: 18,
+  },
+  rpcUrls: {
+    default: {
+      http: ["https://rpc-opal.unique.network"],
+      websocket: ["wss://ws-opal.unique.network"],
+    },
+    public: {
+      http: ["https://rpc-opal.unique.network"],
+      websocket: ["wss://ws-opal.unique.network"],
+    },
+  },
+  blockExplorers: {
+    default: { name: "Opal", url: "https://opal.subscan.io/" },
+  },
+  testnet: true,
+};
 const config = getDefaultConfig({
   appName: 'vent_it', // Đặt tên cho app của bạn
   projectId: "b735f0d8b8e242fb3e26f7c8dd1062b1", // Nhập vào WalletConnect Project ID ở đây
@@ -33,11 +109,21 @@ const config = getDefaultConfig({
   ],
   chains: [
     mainnet,
-    sepolia
+    sepolia,
+    klaytn,
+    klaytnBaobab,
+    uniqueChain,
+    quartzMainnet,
+    opalTestnet,
   ],
   transports: {
+    [8880]: http("https://rpc.unique.network"),
+    [8881]: http("https://rpc-quartz.unique.network"),
+    [8882]: http("https://rpc-opal.unique.network"),
     [mainnet.id]: http('https://rpc.ankr.com/eth'), // Lựa chọn RPC provider là Ankr thay vì mặc định
     [sepolia.id]: http('https://rpc.ankr.com/eth_sepolia'), // Lựa chọn RPC provider là Ankr thay vì mặc định
+    [klaytn.id]: http('https://rpc.ankr.com/klaytn'),
+    [klaytnBaobab.id]: http('https://rpc.ankr.com/klaytn_baobab'),
   },
   ssr: true, // Vì là App router của Nextjs thì bạn cần khai báo ssr là true
 });
