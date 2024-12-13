0aed77cca313c0497acc0e69402972e07e3310c6
Menhythien <dinhthienmenh1505@gmail.com>
2024-12-13 18:09:01 +0700

    add market datum

---
+++ src/ validators/datum.ts
@@ -0,0 +1,13 @@
+import { Data } from "lucid-cardano"
+
+const marketDatumSchema = Data.Object({
+    owner: Data.Bytes(),
+    policyId: Data.Bytes(),
+    assetName: Data.Bytes(),
+    seller: Data.Bytes(),
+    price: Data.Integer()
+})
+
+type marketDatum = Data.Static<typeof marketDatumSchema>;
+
+export const marketDatum = marketDatumSchema as unknown as marketDatum
